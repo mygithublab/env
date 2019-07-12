0ccb3c37538fae35f76a39d0da319c8cd227ba2c
C5158725 <samson.mei@sap.com>
2019-07-12 10:41:25 +0800

    20190712104125

--- 1.repository-local/centos_yum_update.sh
+++ 1.repository-local/centos_yum_update.sh
@@ -3,12 +3,12 @@
 echo 'Updating AliCloud CentOS7 Repository Source'
 DATETIME=`date +%Y-%m-%d_%H-%M-%S`
 exec > /var/log/AliCloud_CentOS7_Repo_$DATETIME.log
-   reposync -np /mirror
+   reposync -np /mirror/centos7
 if [ $? -eq 0 ]; then
-   createrepo --update /mirror/base
-   createrepo --update /mirror/extras
-   createrepo --update /mirror/updates
-   createrepo --update /mirror/epel
+   createrepo --update /mirror/centos7/base
+   createrepo --update /mirror/centos7/extras
+   createrepo --update /mirror/centos7/updates
+   createrepo --update /mirror/centos7/epel
   echo "SUCCESS: $DATETIME AliCloud CentOS7 Repository Update successful"
  else
   echo "ERROR: $DATETIME AliCloud CentOS7 Repository Update failed"
