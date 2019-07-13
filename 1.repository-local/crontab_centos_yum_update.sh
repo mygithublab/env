3aca305e2b14c168456147aeb7f1b914cd987aa4
C5158725 <samson.mei@sap.com>
2019-07-13 17:36:37 +0800

    20190713173636

--- 1.repository-local/crontab_centos_yum_update.sh
+++ 1.repository-local/crontab_centos_yum_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo sed -i '$a */10 * * * * root /bin/bash /vagrant/centos_yum_update.sh' /etc/crontab
+sudo sed -i '$a */30 * * * * root /bin/bash /vagrant/centos_yum_update.sh' /etc/crontab
 
 #sed -i '$a 00 13 * * 6 [ $(date +%d) -eq $(cal | awk 'NR==3{PRINT $NF}')] && /bin/bash /vagrant/crontab_centos_yum_update.sh' /etc/crontab
